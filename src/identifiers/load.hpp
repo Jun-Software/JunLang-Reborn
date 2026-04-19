d1a1f33717df51f1cf5d192be11425857affbc84
lemonorangeapple <jcj1947725596@hotmail.com>
2026-04-19 13:08:05 +0800

    feat: 提供加载whl的方式 bug: 作用域产生问题，无法跨文件运行函数

--- src/identifiers/load.hpp
+++ src/identifiers/load.hpp
@@ -5,21 +5,31 @@
 **/
 void load(vector<string>::iterator it, vector<vector<string> > lines, int &line) {
     string next = *(it + 1);
+    string packageType = split(next)[0];
+    string packageId = split(next)[1];
+    if (packageType != "lib" && packageType != "whl") {
+        cerr << "[Error, " << line << "] Invalid package type: " << packageType << endl;
+        exit(0);
+    }
     if (!libload) {
         cerr << "[Error, " << line << "] Your device is not support to load library" << endl;
         exit(0);
     }
-    string libPath = "packages/" + next + ".junlib";
+    string libPath = "packages/" + packageId + ".jun" + packageType;
+    if (packageType == "whl") {
+        read(libPath.c_str());
+        return;
+    }
 #ifdef _WIN32
     HMODULE hDll = LoadLibrary(libPath.c_str());
     if (hDll == NULL) {
-        cerr << "[Error, " << line << "] Load " << next << ": " << GetLastError() << endl;
+        cerr << "[Error, " << line << "] Load " << packageId << ": " << GetLastError() << endl;
         exit(0);
     }
 #elif __linux__
     void* hDll = dlopen(libPath.c_str(), RTLD_LAZY);
     if (!hDll) {
-        cerr << "[Error, " << line << "] Load " << next << ": " << dlerror() << endl;
+        cerr << "[Error, " << line << "] Load " << packageId << ": " << dlerror() << endl;
         exit(0);
     }
 #endif
@@ -28,7 +38,7 @@ void load(vector<string>::iterator it, vector<vector<string> > lines, int &line)
 #ifdef _WIN32
     loadFunc pAdd = (loadFunc)GetProcAddress(hDll, "load");
     if (pAdd == NULL) {
-        cerr << "[Error, " << line << "] Load " << next << ": " << GetLastError() << endl;
+        cerr << "[Error, " << line << "] Load " << packageId << ": " << GetLastError() << endl;
         FreeLibrary(hDll);
         exit(0);
     }
@@ -36,7 +46,7 @@ void load(vector<string>::iterator it, vector<vector<string> > lines, int &line)
     loadFunc pAdd = (loadFunc)dlsym(hDll, "load");
     const char* error = dlerror();
     if (error != NULL) {
-        cerr << "[Error, " << line << "] Load " << next << ": " << error << endl;
+        cerr << "[Error, " << line << "] Load " << packageId << ": " << error << endl;
         dlclose(hDll);
         exit(0);
     }
